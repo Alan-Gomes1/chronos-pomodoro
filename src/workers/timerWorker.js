1d1c78f554ca56515a461b113974f612771e1f0b
Alan Gomes <alan.gomes.ag28@gmail.com>
2026-01-24 12:49:22 -0300

    feat: Adds web worker for task countdown timer

---
+++ src/workers/timerWorker.js
@@ -0,0 +1,24 @@
+let isRunning = false;
+
+self.onmessage = function (e) {
+  if (isRunning) return;
+
+  isRunning = true;
+
+  const state = e.data;
+  const { activeTask, secondsRemaining } = state;
+  const endTime = activeTask.startDate + secondsRemaining * 1000;
+  const now = Date.now();
+  let countDownSeconds = Math.ceil((endTime - now) / 1000);
+
+  function tick() {
+    self.postMessage(countDownSeconds);
+
+    const now = Date.now();
+    countDownSeconds = Math.floor((endTime - now) / 1000);
+
+    setTimeout(tick, 1000);
+  }
+
+  tick();
+};
