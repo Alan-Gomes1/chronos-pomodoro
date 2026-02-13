7c326b7c618ada7a9fc5ec9a1491e552d8c809ed
Alan Gomes <alan.gomes.ag28@gmail.com>
2026-02-13 18:26:30 -0300

    feat: Adds support for changing task settings

--- src/contexts/TaskContext/taskActions.ts
+++ src/contexts/TaskContext/taskActions.ts
@@ -1,4 +1,5 @@
 import type { TaskModel } from '../../components/models/TaskModel';
+import type { TaskStateModel } from '../../components/models/TaskStateModel';
 
 export enum TaskActionTypes {
   START_TASK = 'START_TASK',
@@ -6,6 +7,7 @@ export enum TaskActionTypes {
   RESET_TASKS = 'RESET_TASKS',
   COUNT_DOWN = 'COUNT_DOWN',
   COMPLETE_TASK = 'COMPLETE_TASK',
+  CHANGE_SETTINGS = 'CHANGE_SETTINGS',
 }
 
 export type TaskActionModel =
@@ -16,6 +18,10 @@ export type TaskActionModel =
   | {
       type: TaskActionTypes.COUNT_DOWN;
       payload: { secondsRemaining: number };
+    }
+  | {
+      type: TaskActionTypes.CHANGE_SETTINGS;
+      payload: TaskStateModel['config'];
     }
   | {
       type: TaskActionTypes.INTERRUPT_TASK;
